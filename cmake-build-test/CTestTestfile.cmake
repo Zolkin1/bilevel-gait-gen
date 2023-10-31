be46f05d2432e4b67ca5f9168ffe2a7dab9f6cdd
zach <zach.olkin@gmail.com>
2023-10-31 16:10:27 -0700

    added some (very) basic unit tests.

---
+++ cmake-build-test/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/zach/AmberLab/bilevel-gait-generation/bilevel-gait-gen
+# Build directory: /home/zach/AmberLab/bilevel-gait-generation/bilevel-gait-gen/cmake-build-test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("simulation")
+subdirs("test")
+subdirs("src")
+subdirs("apps")
