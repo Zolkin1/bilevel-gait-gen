9b70c2ab5ab03c2215930fa3c88e433956147dea
zach <zach.olkin@gmail.com>
2023-11-01 19:17:00 -0700

    update the git ignore

---
+++ simulation/include/qp_control.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2023 Zachary Olkin. All rights reserved.
+//
+
+#ifndef BILEVEL_GAIT_GEN_QP_CONTROL_H
+#define BILEVEL_GAIT_GEN_QP_CONTROL_H
+
+#include "controller.h"
+
+namespace simulator {
+    class QPControl {
+
+    };
+}   // simulator
+
+
+#endif //BILEVEL_GAIT_GEN_QP_CONTROL_H
---
+++ simulation/qp_control.cpp
@@ -0,0 +1,5 @@
+//
+// Copyright (c) 2023 Zachary Olkin. All rights reserved.
+//
+
+#include "qp_control.h"
